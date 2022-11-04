4ce487b57938e2f81f492d338203e5f7f5a863a5
tribagus <tri.bagus21@gmail.com>
2022-11-04 17:32:15 +0700

    assertions and matchers

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -8,13 +8,13 @@ import { Greet } from "./greet"
 describe("Greet", () => {
   test("renders correctly", () => {
     render(<Greet />)
-    const textElement = screen.getByText("Hello")
+    const textElement = screen.getByText(/Hello/)
     expect(textElement).toBeInTheDocument()
   })
 
-  test("renders a name", () => {
-    render(<Greet name="Bagus" />)
-    const textElement = screen.getByText("Hello Bagus")
-    expect(textElement).toBeInTheDocument()
-  })
+  //   test("renders a name", () => {
+  //     render(<Greet name="Bagus" />)
+  //     const textElement = screen.getByText("Hello Bagus")
+  //     expect(textElement).toBeInTheDocument()
+  //   })
 })
