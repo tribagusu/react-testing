3bff29ed805a43428603024d0478fe98aa868966
tribagus <tri.bagus21@gmail.com>
2022-11-04 14:29:09 +0700

    test driven development

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,8 +1,18 @@
+/**
+ * Greet should render the text hello and if a name is passed into the component It should render hello followed by the name
+ */
+
 import { render, screen } from "@testing-library/react"
 import { Greet } from "./greet"
 
 test("Greet renders correctly", () => {
-  render(<Greet />) // get virtual dom / component
-  const textElement = screen.getByText(/hello/i) // get the unit that want to be tested
-  expect(textElement).toBeInTheDocument() // compare to the expected result
+  render(<Greet />)
+  const textElement = screen.getByText("Hello")
+  expect(textElement).toBeInTheDocument()
+})
+
+test("Greet renders with a name", () => {
+  render(<Greet name="Bagus" />)
+  const textElement = screen.getByText("Hello Bagus")
+  expect(textElement).toBeInTheDocument()
 })
--- src/components/greet/greet.tsx
+++ src/components/greet/greet.tsx
@@ -1,3 +1,7 @@
-export const Greet = () => {
-  return <div>Hello</div>
+type GreetProps = {
+  name?: string
+}
+
+export const Greet = (props: GreetProps) => {
+  return <div>Hello {props.name} </div>
 }
