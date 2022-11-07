715ec833c5324b051e1ca30834990840ef3e8dca
tribagus <tri.bagus21@gmail.com>
2022-11-07 11:30:31 +0700

    query method

---
+++ src/components/application/application.test.tsx
@@ -0,0 +1,53 @@
+import { render, screen } from "@testing-library/react"
+import { Application } from "./application"
+
+describe("Application", () => {
+  test("renders correctly", () => {
+    render(<Application />)
+
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+      name: "Job application form",
+    })
+    expect(pageHeading).toBeInTheDocument()
+
+    const sectionHeading = screen.getByRole("heading", {
+      level: 2,
+      name: "Section 1",
+    })
+    expect(sectionHeading).toBeInTheDocument()
+
+    //# by Role
+    // const nameElement = screen.getByRole("textbox", {
+    //   name: "Name",
+    // })
+    // expect(nameElement).toBeInTheDocument()
+
+    //# by LabelText with Selector
+    const nameElement2 = screen.getByLabelText("Name", {
+      selector: "input",
+    })
+    expect(nameElement2).toBeInTheDocument()
+
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    })
+    expect(bioElement).toBeInTheDocument()
+
+    const jobLocationElement = screen.getByRole("combobox")
+    expect(jobLocationElement).toBeInTheDocument()
+
+    //# by Role
+    // const termsElement = screen.getByRole("checkbox")
+    // expect(termsElement).toBeInTheDocument()
+
+    //# by LabelText
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    )
+    expect(termsElement2).toBeInTheDocument()
+
+    const submitButtonElement = screen.getByRole("button")
+    expect(submitButtonElement).toBeInTheDocument()
+  })
+})
