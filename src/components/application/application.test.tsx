bd5dc6a7de49e894acd252bcc33d86b2a8363370
tribagus <tri.bagus21@gmail.com>
2022-11-08 22:24:16 +0700

    eslint and prettier

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -71,5 +71,6 @@ describe("Application", () => {
 
     const submitButtonElement = screen.getByRole("button")
     expect(submitButtonElement).toBeInTheDocument()
+    expect(submitButtonElement).toBeDisabled()
   })
 })
